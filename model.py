01c914a13470dd945472f14993d3b36ab85db11e
YehSweeKhim <sweekhim_yeh@mymail.sutd.edu.sg>
2021-08-09 01:32:46 +0000

    Updated model

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ import torchvision.models as models
 from torch.autograd import Variable
 from basic_layers import ResidualBlock
 from attention_module import AttentionModule_stage1, AttentionModule_stage2, AttentionModule_stage3, AttentionModule_stage0
+from attention_module import AttentionModule_stage1_cifar, AttentionModule_stage2_cifar, AttentionModule_stage3_cifar
 
 
 def convrelu(in_channels, out_channels, kernel, padding):
@@ -25,12 +26,15 @@ class ResNet_Attention_UNet(nn.Module):
         self.base_layers = list(self.base_model.children())
 
         self.layer0 = nn.Sequential(*self.base_layers[:3]) # size=(N, 64, x.H/2, x.W/2)
-        self.layer1 = nn.Sequential(*self.base_layers[3:5]) # size=(N, 64, x.H/4, x.W/4)
-        self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
-        self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
-        self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
-        
         self.layer0_1x1 = convrelu(64, 64, 1, 0)
+        self.layer1 = nn.Sequential(*self.base_layers[3:5]) # size=(N, 64, x.H/4, x.W/4)
+        self.layer1_1x1 = convrelu(64, 64, 1, 0)
+        self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
+        self.layer2_1x1 = convrelu(128, 128, 1, 0)
+        self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
+        self.layer3_1x1 = convrelu(256, 256, 1, 0)
+        self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
+        self.layer4_1x1 = convrelu(512, 512, 1, 0)
         
         self.residual_block4 = ResidualBlock(512, 512)
         self.attention_module4 = AttentionModule_stage3(512, 512)
@@ -53,7 +57,7 @@ class ResNet_Attention_UNet(nn.Module):
         self.conv_original_size2 = convrelu(64 + 128, 64, 3, 1)
 
         self.conv_last = nn.Conv2d(64, 5, 1)
-
+        
     # Model with 2 layers
     def forward(self, input):
         x_original = self.conv_original_size0(input)
@@ -126,7 +130,8 @@ class ResNet_Attention_UNet(nn.Module):
 
     #     out = self.conv_last(x)
     #     return out
-
+    
+    
     
 class EfficientNet_Attention_UNet(nn.Module):
     def __init__(self, model):
@@ -135,6 +140,10 @@ class EfficientNet_Attention_UNet(nn.Module):
         self.base_model = model.from_pretrained('efficientnet-b7')
 
         self.layer0_1x1 = convrelu(64, 64, 1, 0)
+        self.layer1_1x1 = convrelu(64, 64, 1, 0)
+        self.layer2_1x1 = convrelu(128, 128, 1, 0)
+        self.layer3_1x1 = convrelu(256, 256, 1, 0)
+        self.layer4_1x1 = convrelu(512, 512, 1, 0)
         
         self.residual_block4 = ResidualBlock(512, 512)
         self.attention_module4 = AttentionModule_stage3(512, 512)
